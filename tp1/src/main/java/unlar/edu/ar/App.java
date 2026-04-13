e08ab1b4904026aab66b2c97026dd92433c0226d
gustavod25 <gustavodf25@gmail.com>
2026-04-13 14:35:27 -0300

    primeros pasos

--- tp1/src/main/java/unlar/edu/ar/App.java
+++ tp1/src/main/java/unlar/edu/ar/App.java
@@ -1,7 +1,11 @@
 package unlar.edu.ar;
 
+import unlar.edu.ar.biblioteca.Biblioteca;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Biblioteca sistema = new Biblioteca();
+
     }
+
 }
--- tp1/src/main/java/unlar/edu/ar/biblioteca/Biblioteca.java
+++ tp1/src/main/java/unlar/edu/ar/biblioteca/Biblioteca.java
@@ -1,5 +1,18 @@
 package unlar.edu.ar.biblioteca;
 
+import unlar.edu.ar.objets.Estudiante;
+
 public class Biblioteca {
+    private List<Libro> catalogo = new Arraylist<>();// es una lista de libros
+    private Map<String, estudiante> estudiantes = new HashMap<>();// obtenemos el hash del estudiante
+    private set<Prestamo> prestamosActivos = new HashSet<>();// hash del pretamo
+
+    public void agregarLibro(Libro l) {
+        catalogo.add(l);
+    } // agregas libros
+
+    public void agregarEstudiante(Estudiante e) {
+        estudiantes.put(e.getLegajo(), e);
+    }// usamos el numero de legajo para guardar al pibe
 
 }
